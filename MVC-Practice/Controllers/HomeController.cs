2aa400aca84e8374da6ab66b6a89f99780d6e7b6
ipz224_tmd <ipz224_tmd@student.ztu.edu.ua>
2025-04-25 01:55:39 +0300

    Add project files.

---
+++ MVC-Practice/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MVC_Practice.Models;
+
+namespace MVC_Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Greeting(NameModel model)
+        {
+            var name = string.IsNullOrWhiteSpace(model.Name) ? "Unknown" : model.Name;
+            ViewData["Username"] = model.Name;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC-Practice/Models/NameModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC_Practice.Models
+{
+    public class NameModel
+    {
+        public string? Name { get; set; }
+    }
+}
