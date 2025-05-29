2957959d09ef8eeca3bc818b042bccb8e5bfb3d2
ipz224_tmd <ipz224_tmd@student.ztu.edu.ua>
2025-05-29 18:52:30 +0300

    Add factory for repositoriries

---
+++ MVC-Practice/Repositories/Implementations/RepositoryFactory.cs
@@ -0,0 +1,19 @@
+﻿using MVC_Practice.Repositories.Interfaces;
+
+namespace MVC_Practice.Repositories.Implementations
+{
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        private readonly ITodoRepository _dbRepo;
+        private readonly ITodoRepository _xmlRepo;
+        public RepositoryFactory(ITodoRepository dbRepo, ITodoRepository xmlRepo)
+        {
+            _dbRepo = dbRepo;
+            _xmlRepo = xmlRepo;
+        }
+        public ITodoRepository CreateTodoRepository(string storageType)
+        {
+            return storageType == "xml" ? _xmlRepo : _dbRepo;
+        }
+    }
+}
---
+++ MVC-Practice/Repositories/Implementations/TodoXMLRepository.cs
@@ -0,0 +1,38 @@
+﻿using MVC_Practice.Models;
+using MVC_Practice.Repositories.Interfaces;
+
+namespace MVC_Practice.Repositories.Implementations
+{
+    public class TodoXMLRepository : ITodoRepository
+    {
+        public Task AddCategoryAsync(Categories category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddTaskAsync(Tasks task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CompleteTask(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tasks>> GetActiveTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Categories>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tasks>> GetCompletedTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
